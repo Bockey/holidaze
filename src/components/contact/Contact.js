23d16549d2506c88c9ab9771709a544842f1548e
Boris Gudelj <71261529+Bockey@users.noreply.github.com>
2023-04-06 12:01:08 +0200

    Update Contact.js

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -65,7 +65,7 @@ export default function Contact() {
     };
     try {
       const response = await axios.post(
-        "https://noroffcors.herokuapp.com/https://holidaze.bockey.one/wp-json/wp/v2/comments?post=249",
+        "https://corsproxy.io/?https://holidaze.bockey.one/wp-json/wp/v2/comments?post=249",
         newData
       );
       console.log("response", response.data);
